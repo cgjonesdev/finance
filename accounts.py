8c3ecdd6564611b83a598d3af0b7d325c97197e8
Cary Jones <cgjonesdev@gmail.com>
2015-10-01 18:24:18 -0500

    Updated accounts module

--- accounts.py
+++ accounts.py
@@ -1,26 +1,124 @@
 class Account(object):
-    pass
+    name = ''
+    balance = 0.0
+
+    def __str__(self):
+        return self.__class__.__name__.lower()
+
+    def __add__(self, other):
+        if 'balance' in self.__dict__:
+            self.__dict__['balance'] += other.balance
+        if 'amount' in self.__dict__:
+            self.__dict__['amount'] += other.amount
+        if 'limit' in self.__dict__:
+            self.__dict__['limit'] += other.limit
+        return self
+
+    def __radd__(self, other):
+        if 'balance' in self.__dict__:
+            self.__dict__['balance'] += other.balance
+        if 'amount' in self.__dict__:
+            self.__dict__['amount'] += other.amount
+        if 'limit' in self.__dict__:
+            self.__dict__['limit'] += other.limit
+        return self
+
+    def __sub__(self, other):
+        if 'balance' in self.__dict__:
+            self.__dict__['balance'] -= other.balance
+        if 'amount' in self.__dict__:
+            self.__dict__['amount'] -= other.amount
+        if 'limit' in self.__dict__:
+            self.__dict__['limit'] -= other.limit
+        return self
+
+    def __rsub__(self, other):
+        if 'balance' in self.__dict__:
+            self.__dict__['balance'] -= other.balance
+        if 'amount' in self.__dict__:
+            self.__dict__['amount'] -= other.amount
+        if 'limit' in self.__dict__:
+            self.__dict__['limit'] -= other.limit
+        return self
+
+    def __mul__(self, other):
+        if 'balance' in self.__dict__:
+            self.__dict__['balance'] *= other.balance
+        if 'amount' in self.__dict__:
+            self.__dict__['amount'] *= other.amount
+        if 'limit' in self.__dict__:
+            self.__dict__['limit'] *= other.limit
+        return self
+
+    def __rmul__(self, other):
+        if 'balance' in self.__dict__:
+            self.__dict__['balance'] *= other.balance
+        if 'amount' in self.__dict__:
+            self.__dict__['amount'] *= other.amount
+        if 'limit' in self.__dict__:
+            self.__dict__['limit'] *= other.limit
+        return self
+
+    def __div__(self, other):
+        if 'balance' in self.__dict__:
+            self.__dict__['balance'] /= other.balance
+        if 'amount' in self.__dict__:
+            self.__dict__['amount'] /= other.amount
+        if 'limit' in self.__dict__:
+            self.__dict__['limit'] /= other.limit
+        return self
+
+    def __rdiv__(self, other):
+        if 'balance' in self.__dict__:
+            self.__dict__['balance'] /= other.balance
+        if 'amount' in self.__dict__:
+            self.__dict__['amount'] /= other.amount
+        if 'limit' in self.__dict__:
+            self.__dict__['limit'] /= other.limit
+        return self
 
 
 class Receivable(Account):
-    pass
+    amount = 0.0
 
 
 class Payable(Account):
-    pass
+    amount = 0.0
 
 
 class Checking(Account):
-    pass
+    nickname = ''
 
 
 class Savings(Account):
-    pass
+    nickname = ''
 
 
 class Credit(Account):
-    pass
+    limit = 0.0
+    interest_rate = 0.0
 
 
 class Investment(Account):
     pass
+
+
+r = Receivable()
+r.amount = 500
+
+p = Payable()
+p.amount = 100
+
+print (r / p).amount
+
+c1 = Credit()
+c2 = Credit()
+
+c1.limit = 5000
+c1.balance = 100
+c2.limit = 1000
+c2.balance = 500
+
+print (c1 - c2).__dict__
+
+print c1 - p
--- transactions.py
+++ transactions.py
@@ -10,6 +10,14 @@ class Debit(Transaction):
     pass
 
 
+class Deposit(Transaction):
+    pass
+
+
+class Payment(Transaction):
+    pass
+
+
 class Transfer(Transaction):
     pass
 

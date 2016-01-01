4a4ce067d16c7489c2b636ebe98f053fb03a883c
Cary Jones <cgjonesdev@gmail.com>
2016-01-01 00:47:37 -0600

    Added subclass to accounts base

--- code/accounts/base.py
+++ code/accounts/base.py
@@ -1,6 +1,7 @@
 from pprint import pprint, pformat
 from datetime import datetime
 from code.base import BaseMany, BaseSingleton
+from code.mixins import Magic
 
 
 class Accounts(BaseMany):
@@ -11,10 +12,18 @@ class Accounts(BaseMany):
         self.__dict__.update(self.data)
         for i, item in enumerate(self.items):
             self.items[i] = AccountSingleton(item)
+            if 'contact' in item:
+                self.items[i].contact = Contact(item['contact'])
 
 
 class AccountSingleton(BaseSingleton):
     pass
+
+
+class Contact(Magic):
+
+    def __init__(self, contact_dict):
+        self.__dict__.update(contact_dict)
 
 
 if __name__ == '__main__':

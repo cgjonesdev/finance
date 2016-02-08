556f0ec4ec98722e16ef32f08b793f2254182c4c
Cary Jones <cary.jones@rackspace.com>
2016-02-08 09:02:43 -0600

    Updated pay deductions data

--- code/pay_deductions.py
+++ code/pay_deductions.py
@@ -1,5 +1,6 @@
 import sys
 from pprint import pprint, pformat
+from datetime import datetime as dt
 from mixins import IO
 
 
@@ -18,12 +19,16 @@ data = [
             'chase overdraft': -322.18,
             'pay': 1460.01,
             'ford': -462.08,
-            'gas': -24.53,
+            'gas': sum([-24.53, -15.81, -19.70]),
             'car supplies': -14.05,
-            'snacks': -3.83,
+            'snacks': sum([-3.83, -2.15, -4.20, -2.15, -2.71]),
             'chase ink': -76.00,
             'post office': -13.78,
-            'premier bank card': -97.0
+            'premier bank card': -97.0,
+            'costco': -141.08,
+            'itunes': -1.07,
+            'groceries': -2.51,
+            'food': sum([-2.79, -8, -5.57, -5.15, -10.5])
         }
     },
     {
@@ -31,10 +36,10 @@ data = [
         'estimated': {
             'pay': 1460.01,
             'rich dad education': -500.0,
-            'costco': -200.0,
             'chase business overdraft': -232.15,
             'chase ink': -116.00,
-            'bofa overdraft': -182.52
+            'renters insurance': -220,
+            'cps energy': -64.47
         },
         'actual': {'pay': 1460.01}
     },
@@ -42,7 +47,18 @@ data = [
         'date': '2-19-16',
         'estimated': {
             'pay': 1460.01,
-            'credit cards': -500.0,
+            'credit cards': -200.0,
+            'toll bill': sum([-83.84, -37.41]),
+            'bofa overdraft': -182.52,
+            'ford': -600
+        },
+        'actual': {'pay': 1460.01}
+    },
+    {
+        'date': '2-26-16',
+        'estimated': {
+            'pay': 1460.01,
+            'rent': -1133,
         },
         'actual': {'pay': 1460.01}
     }
@@ -80,11 +96,14 @@ class Deductions(IO):
     def _prepare_output(self, key, output):
         self.total = data[self.index][key]['pay']
         data[self.index][key]['savings'] = -1 * round(self.total * .1, 2)
-        data[self.index][key]['misc'] = -1 * round(self.total * .05, 2)
+        if self.index != 0:
+            data[self.index][key]['misc'] = -1 * round(self.total * .06849, 2)
+        else:
+            data[self.index][key]['misc'] = -1 * round(self.total * .05, 2)
         self.max_len_names = max([len(k) for k in data[self.index][key].keys()]) + 7
         output += 'Name'.ljust(self.max_len_names) + 'Amount'.ljust(14)  + 'Balance\n'
         output += '' + '-' * (self.max_len_names + 22) + '\n'
-        for k, v in data[self.index][key].items():
+        for k, v in sorted(data[self.index][key].items()):
             if k not in ('pay', 'date'):
                 output += '{}${}${}\n'.format(
                     k.ljust(self.max_len_names),
@@ -115,7 +134,11 @@ class Main(object):
 
     @classmethod
     def all(cls):
-        list(Deductions())
+        output = ''.join(list(Deductions()))
+        print output
+        with open('logs/pay/deductions_all_{}.txt'.format(
+            dt.now().strftime('%a_%b_%d_%Y')), 'w') as out:
+            out.write(output)
 
     @classmethod
     def single(cls):
